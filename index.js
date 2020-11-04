f2ba0e528d4a28e6a59a9a7d2cc4518f6b1740bc
unknown <jessop.lucas@gmail.com>
2020-11-04 15:57:24 -0700

    finished node3

---
+++ index.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const express = require('express');
+const massive = require('massive')
+const ctrl = require("./products_controller")
+
+
+const app = express();
+
+const{SERVER_PORT, CONNECTION_STRING} = process.env;
+
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {
+        rejectUnauthorized: false
+    }
+}).then(dbInstance => {
+    app.set('db',dbInstance)
+    console.log("Connected to port")
+}).catch(err => console.log(err))
+
+app.get('/api/products', ctrl.getAll)
+app.get('/api/products/:id', ctrl.getOne)
+app.put('/api/products/:id', ctrl.update)
+app.post('/api/products', ctrl.create)
+app.delete('/api/products/:id', ctrl.delete)
+
+
+
+
+app.listen(SERVER_PORT, () => {
+    console.log(`Server listening on port ${SERVER_PORT}.`)
+})
